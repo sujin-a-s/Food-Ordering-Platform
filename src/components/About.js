03ac006c3f39ed04d9b355daeb6b95942a8aab8a
Sujin <sunilkumarsujin@gmail.com>
2023-11-08 15:17:46 +0530

    UserContext in class based components

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import ProfileFunctional from './ProfileFunctional.js';
 import ProfileClass from './ProfileClass.js';
 import { Component } from 'react';
+import UserContext from '../utils/UserContext.js';
 
 class About extends Component {
   constructor(props) {
@@ -19,6 +20,12 @@ class About extends Component {
     return (
       <div>
         <h2>About Us Page</h2>
+
+        <UserContext.Consumer>
+          {({user}) => {
+            return <h4 className="font-bold text-xl p-10">{user.name}-{user.email}</h4>
+          }}
+        </UserContext.Consumer>
         
         <ProfileClass />
         
